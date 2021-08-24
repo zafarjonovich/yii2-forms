f0e2eb44e6f79146b617869c15c69e45be7856ba
zafarjonovich <adashaliyev50@gmail.com>
2021-08-24 21:14:27 +0300

    Added new features

--- src/forms/BaseForm.php
+++ src/forms/BaseForm.php
@@ -4,9 +4,42 @@
 namespace zafarjonovich\Yii2Forms\forms;
 
 
+use app\models\Person;
 use yii\base\Model;
+use yii\db\ActiveRecord;
+use yii\helpers\ArrayHelper;
 
 class BaseForm extends Model
 {
+    /**
+     * @var ActiveRecord $activeRecordModel
+     */
+    public $activeRecordModel;
 
+    protected $attributes = [];
+
+    public function init()
+    {
+        if(get_parent_class(Person::class) != ActiveRecord::class){
+            throw new \Exception('activeRecordModel must instance of ActiveRecord');
+        }
+    }
+
+    public function save($runValidation = true, $attributeNames = null)
+    {
+        /**
+         * @var ActiveRecord $activeRecordModel
+         */
+        $activeRecordModel = new $this->activeRecordModel;
+
+        $attributeNames = array_intersect($activeRecordModel->attributes(),$this->attributes());
+
+        //dump($attributeNames);die;
+
+        \Yii::configure($activeRecordModel,$this->getAttributes($attributeNames));
+
+        $activeRecordModel->save($runValidation,$attributeNames);
+
+        return $activeRecordModel;
+    }
 }
--- src/forms/BaseNestedForm.php
+++ src/forms/BaseNestedForm.php
@@ -3,15 +3,28 @@
 
 namespace zafarjonovich\Yii2Forms\forms;
 
-use zafarajonovich\Yii2Forms\forms\BaseForm;
+use zafarjonovich\Yii2Forms\forms\BaseForm;
 use yii\base\Exception;
 use yii\base\Model;
 
 class BaseNestedForm extends BaseForm
 {
-    protected $nestedForms = [];
+    private $multipleNestedForms = [];
+
+    private $nestedForms = [];
 
     private $skipableProperties = [];
+
+    public function init()
+    {
+        foreach ($this->nestedForms as $property => $options) {
+            if(!$this->isMultipleForm($property)){
+                $this->{$property} = \Yii::createObject($options['formClass'],$options['initialProperties']);
+            }
+        }
+
+        parent::init();
+    }
 
     /**
      * @param $name
@@ -40,6 +53,16 @@ class BaseNestedForm extends BaseForm
         return $this->isSkipableProperty($property) and !isset($data[$property]);
     }
 
+    private function isMultipleForm($property_name)
+    {
+        return in_array($property_name,$this->multipleNestedForms);
+    }
+
+    public function hasNestedForm($property_name)
+    {
+        return isset($this->nestedForms[$property_name]);
+    }
+
     /**
      * @return bool
      */
@@ -52,10 +75,21 @@ class BaseNestedForm extends BaseForm
      * @param $property_name
      * @param $formClass
      */
-    public function addNestedForm($property_name, $formClass)
+    public function addNestedForm($property_name, $formClass,$initialProperties = [])
     {
-        $this->nestedForms[$property_name] = $formClass;
+        $this->nestedForms[$property_name] = compact('formClass','initialProperties');
     }
+
+    /**
+     * @param $property_name
+     * @param $formClass
+     */
+    public function addMultipleNestedForm($property_name, $formClass, $initialProperties = [])
+    {
+        $this->addNestedForm($property_name, $formClass, $initialProperties);
+        $this->multipleNestedForms[] = $property_name;
+    }
+
 
     /**
      * @param array $values
@@ -88,7 +122,7 @@ class BaseNestedForm extends BaseForm
      */
     private function initializeNestedForms($data)
     {
-        foreach ($this->nestedForms as $property => $class) {
+        foreach ($this->nestedForms as $property => $options) {
 
             if (!$this->hasProperty($property)) {
                 throw new Exception("Property {$property} does not exists in class ");
@@ -97,16 +131,35 @@ class BaseNestedForm extends BaseForm
             if($this->canSkip($property,$data))
                 continue;
 
-            if(!is_array($data[$property]))
-                throw new Exception("Property {$property} should be in array format");
+            if(!isset($data[$property]))
+                throw new Exception("Property {$property} should set");
 
-            /**
-             * @var $model Model
-             */
-            foreach ($data[$property] as $item) {
-                $model = new $class;
-                $model->setAttributes($item);
-                $this->{$property}[] = $model;
+            $properties = $data[$property];
+
+            if($this->isMultipleForm($property)){
+
+                if(!is_array($properties))
+                    throw new Exception("Property {$property} should be in array format");
+
+                /**
+                 * @var $model Model
+                 */
+                foreach ($properties as $item){
+                    $initialOptions = array_merge($options['initialProperties'],$item);
+
+                    $model = new $options['formClass'];
+                    $model->setAttributes($initialOptions);
+
+                    $this->{$property}[] = $model;
+                }
+            }else{
+
+                $initialOptions = array_merge($options['initialProperties'],$properties);
+
+                $model = new $options['formClass'];
+                $model->setAttributes($initialOptions);
+
+                $this->{$property} = $model;
             }
         }
     }
@@ -122,22 +175,20 @@ class BaseNestedForm extends BaseForm
 
         foreach ($this->nestedForms as $property => $class) {
             if(!$this->isSkipableProperty($property)){
-                /**
-                 * @var $model Model
-                 */
-                $models = $this->{$property};
 
-                foreach ($models as $model) {
-                    if (
-                        ($model->hasMethod('validateNested') && !$model->validateNested()) ||
-                        (!$model->hasMethod('validateNested') && !$model->validate())
-                    ) {
-                        $isOk = false;
-                        $this->addError($property, "Nested Property: {$property} validation error");
-                    }
+                if($this->isMultipleForm($property)){
+                    $ok = Model::validateMultiple($this->{$property}) && $isOk;
+                }else{
+                    $ok = $this->{$property}->validate() && $isOk;
                 }
+
+                if(!$ok)
+                    $this->addError($property,"Nested form error");
+
+                $isOk = $ok && $isOk;
             }
         }
+
         return $isOk;
     }
 }
