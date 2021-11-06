e61d5fc717417008dbfcf3cdab30a75734557a35
zafarjonovich <adashaliyev50@gmail.com>
2021-11-07 01:49:05 +0300

    Upgrated new version

--- src/forms/BaseForm.php
+++ src/forms/BaseForm.php
@@ -4,42 +4,76 @@
 namespace zafarjonovich\Yii2Forms\forms;
 
 
-use app\models\Person;
 use yii\base\Model;
 use yii\db\ActiveRecord;
 use yii\helpers\ArrayHelper;
 
 class BaseForm extends Model
 {
-    /**
-     * @var ActiveRecord $activeRecordModel
-     */
-    public $activeRecordModel;
+    protected $modelProperties = [];
 
-    protected $attributes = [];
-
-    public function init()
+    public static function activeRecordModel()
     {
-        if(get_parent_class(Person::class) != ActiveRecord::class){
-            throw new \Exception('activeRecordModel must instance of ActiveRecord');
-        }
+        return null;
     }
 
-    public function save($runValidation = true, $attributeNames = null)
+    public function getModel()
     {
+        if(get_parent_class(static::activeRecordModel()) != ActiveRecord::class){
+            throw new \Exception('activeRecordModel must instance of ActiveRecord');
+        }
+
+        $modelClass = static::activeRecordModel();
+
+        $where = [];
+
+        foreach ((array)$modelClass::primaryKey() as $primaryKey) {
+            $where[$primaryKey] = $this->{$primaryKey};
+        }
+
         /**
-         * @var ActiveRecord $activeRecordModel
+         * @var ActiveRecord $model
          */
-        $activeRecordModel = new $this->activeRecordModel;
 
-        $attributeNames = array_intersect($activeRecordModel->attributes(),$this->attributes());
+        $model = $modelClass::findOne($where);
 
-        //dump($attributeNames);die;
+        if($model === null) {
+            $model = new $modelClass;
+        }
 
-        \Yii::configure($activeRecordModel,$this->getAttributes($attributeNames));
+        return $model;
+    }
 
-        $activeRecordModel->save($runValidation,$attributeNames);
+    public function __get($name)
+    {
+        return $this->modelProperties[$name] ?? null;
+    }
 
-        return $activeRecordModel;
+    public function __set($name,$value)
+    {
+        if(!in_array($name,$this->getModel()->attributes())) {
+            throw new \Exception('Setting uncnown property: '.(static::activeRecordModel()).'::'.$name);
+        }
+
+        $this->modelProperties[$name] = $value;
+    }
+
+    public function attributes()
+    {
+        return $this->getModel()->attributes();
+    }
+
+    public function configureModel($model)
+    {
+        $attributeNames = array_intersect($model->attributes(),$this->attributes());
+        \Yii::configure($model,$this->getAttributes($attributeNames));
+
+        return $model;
+    }
+
+    public function configure($model)
+    {
+        $attributeNames = array_intersect($this->attributes(),$model->attributes());
+        \Yii::configure($this,$this->getAttributes($attributeNames));
     }
 }
